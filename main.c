e25f12fdbc05c9b7d90298a013475e4e1c401101
unknown <michaelxaviercanonizado@gmail.com>
2025-04-15 19:48:16 +0800

    feat: extract the target notation from the CLI flag

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef enum {
+    INFIX,
+    PREFIX,
+    POSTFIX
+} Notation;
 
 int main(int argc, char *argv[]) {   
     for (int i = 0; i < argc; i++) {
         printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    char *notationFlag = argv[2];
+    Notation targetNotation;
+    if (strcmp(notationFlag, "--infix") == 0) {
+        targetNotation = INFIX;
+    } else if (strcmp(notationFlag, "--prefix") == 0) {
+        targetNotation = PREFIX;
+    } else if (strcmp(notationFlag, "--postfix") == 0) {
+        targetNotation = POSTFIX;
+    } else {
+        printf("\nSpecified target notation is invalid!");
+        printf("\nPlease choose from:");
+        printf("\n   --infix");
+        printf("\n   --prefix");
+        printf("\n   --postfix");
+        printf("\n\n");
     }
     return 0;
 }
